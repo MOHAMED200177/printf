73dbb67d74aa38e96880a0510822bc5f2bce3293
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-25 10:35:43 -0700

    p

---
+++ params.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * countParams - clears struct fields and reset buf
+ * @params: the parameters struct
+ * @args: the argument pointer
+ *
+ * Return: void
+ */
+void countParams(params_t *params, va_list args)
+{
+	params->unsign = 0;
+
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
+
+	params->width = 0;
+	params->precision = UINT_MAX;
+
+	params->h_modifier = 0;
+	params->l_modifier = 0;
+	(void)args;
+}
